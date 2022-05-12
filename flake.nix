db8232328ed81cc37c3c8f66e1a849d8a7d7b067
Marcelo A. de L. Santos <contato.maxhero@gmail.com>
2022-05-12 16:51:44 -0300

    update outputs of flake.nix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   };
 
   outputs =
-    { self, nix-doom-emacs, nixpkgs, home-manager, nix-gaming, ... }@attrs: {
+    { self, nixpkgs, ... }@attrs: {
       nixosConfigurations = {
         "workstation" = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
