45a7c0a49e9f116f5b684db00dfe8aa222a1218f
Marcelo A. de L. Santos <contato.maxhero@gmail.com>
2022-05-12 16:48:41 -0300

    add nowl to it's own file

--- configuration.nix
+++ configuration.nix
@@ -4,20 +4,7 @@
 
 { config, pkgs, nix-gaming, ... }:
 let
-  nowl = (pkgs.writeShellScriptBin "nowl" ''
-    unset CLUTTER_BACKEND
-    unset ECORE_EVAS_ENGINE
-    unset ELM_ENGINE
-    unset SDL_VIDEODRIVER
-    unset BEMENU_BACKEND
-    unset GTK_USE_PORTAL
-    unset NIXOS_OZONE_WL
-    export GDK_BACKEND='x11'
-    export XDG_SESSION_TYPE='x11'
-    export QT_QPA_PLATFORM='xcb'
-    export MOZ_ENABLE_WAYLAND=0
-    exec -a "$0" "$@"
-  '');
+  nowl = (import ./tools/nowl.nix) pkgs;
 in {
   imports = [ ./hardware-configuration.nix ];
 
