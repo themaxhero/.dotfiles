b4e33a1d3d0bb696be9a68d5b7502af3a947daf0
Marcelo A. de L. Santos <contato.maxhero@gmail.com>
2022-06-12 05:35:06 -0300

    add support for controllers on steam

--- configuration.nix
+++ configuration.nix
@@ -189,7 +189,7 @@ in {
     (import (builtins.fetchTarball {
       url =
         "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      sha256 = "1x6kjcf91zvkvbssd922k77xmmi56ik2938rvfcs7y3w09nis3l7";
+      sha256 = "1f724aaycq2z8b9ab0l0f718qbznfljaaqcn8ijnmr5cam7k7v1s";
     }))
   ];
 
@@ -212,6 +212,33 @@ in {
       shell = pkgs.bash;
     };
   };
+
+  # Enable Support for Controllers through steam
+  services.udev.extraRules = ''
+    # This rule is needed for basic functionality of the controller in
+    # Steam and keyboard/mouse emulation
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+    # This rule is necessary for gamepad emulation; make sure you
+    # replace 'pgriffais' with a group that the user that runs Steam
+    # belongs to
+    KERNEL=="uinput", MODE="0660", GROUP="pgriffais", OPTIONS+="static_node=uinput"
+    # Valve HID devices over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+    # Valve HID devices over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
+    # DualShock 4 over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
+    # Dualsense over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0666"
+    # DualShock 4 wireless adapter over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
+    # DualShock 4 Slim over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
+    # DualShock 4 over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
+    # DualShock 4 Slim over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
+  '';
 
   systemd.enableUnifiedCgroupHierarchy = true;
   security.sudo.wheelNeedsPassword = false;
@@ -371,7 +398,11 @@ in {
   programs.dconf.enable = true;
   programs.fish.enable = true;
   programs.gamemode.enable = true;
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
   programs.nm-applet.enable = true;
 
   # Doom Emacs
