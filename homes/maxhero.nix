e438bcfa6c3ec9bd5f2c023acaae69c896152270
Marcelo A. de L. Santos <contato.maxhero@gmail.com>
2022-06-24 13:23:42 -0300

    add wallpaper

--- homes/maxhero.nix
+++ homes/maxhero.nix
@@ -29,7 +29,7 @@ in {
 
   home.file.".zshrc".source = "./.zshrc";
   home.file.".anthy".source = "./.anthy";
-  home.file.".oh-my-zsh".source = "./.oh-my-zsh";
+  home.file.".wallpaper.png".source = "./.wallpaper.png";
   home.file."$XDG_CONFIG_HOME/waybar".source = "waybar";
 
   # I need to setup an IME soon
