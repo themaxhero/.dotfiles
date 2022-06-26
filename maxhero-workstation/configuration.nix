cd8eb1bb7c6ff39f0cf8658454ed2c43f27de88f
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2022-06-26 14:38:39 -0300

    start working on desktop

--- maxhero-workstation/configuration.nix
+++ maxhero-workstation/configuration.nix
@@ -5,4 +5,24 @@
     hostName = "maxhero-workstation";
   };
   services.minidlna.friendlyName = "maxhero-workstation";
+
+  users.users = {
+    maxhero = {
+      isNormalUser = true;
+      uid = 1000;
+      extraGroups = [
+        "wheel"
+        "video"
+        "audio"
+        "realtime"
+        "libvirt"
+        "kvm"
+        "input"
+        "networkmanager"
+        "rtkit"
+        "podman"
+      ];
+      shell = pkgs.bash;
+    };
+  };
 }
