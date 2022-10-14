ef376f80d142a0d008b6889b7c937b5c891da5fd
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2022-10-14 02:25:39 -0300

    fix identation

--- systems/maxhero-workstation/default.nix
+++ systems/maxhero-workstation/default.nix
@@ -3,22 +3,22 @@ nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   specialArgs = attrs;
   modules = [
-  ../../modules/common
-  ../../modules/development
-  ../../modules/gaming
-  ../../modules/networking
-  ../../modules/sound
-  ../../modules/vfio
-  ../../modules/wireguard-client.nix
-  ./configuration.nix
-  ./hardware-configuration.nix
-  home-manager.nixosModules.home-manager
-  ({
-      home-manager.useGlobalPkgs = true;
-      home-manager.users.maxhero = nixpkgs.lib.mkMerge [
-      nix-doom-emacs.hmModule
-      (import ../../home/maxhero { seat = true; })
-      ];
-  })
+    ../../modules/common
+    ../../modules/development
+    ../../modules/gaming
+    ../../modules/networking
+    ../../modules/sound
+    ../../modules/vfio
+    ../../modules/wireguard-client.nix
+    ./configuration.nix
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+    ({
+        home-manager.useGlobalPkgs = true;
+        home-manager.users.maxhero = nixpkgs.lib.mkMerge [
+        nix-doom-emacs.hmModule
+        (import ../../home/maxhero { seat = true; })
+        ];
+    })
   ];
 }
