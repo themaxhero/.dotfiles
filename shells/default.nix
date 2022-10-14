9e70a02106a42a84bc9c471ac42d0e36f083d2c1
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2022-10-14 02:35:40 -0300

    make shells by reading folders inside shells folder

--- shells/default.nix
+++ shells/default.nix
@@ -1,14 +1,17 @@
 { flake-utils, devshell, ...}@attrs:
 (flake-utils.lib.eachDefaultSystem (system:
-    let
-    pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ devshell.overlay ];
-    };
-    in
-    {
-    devShells = {
-        "maintain" = (import ./shells/maintain.nix attrs);
-    };
-    }
+  let
+  pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ devshell.overlay ];
+  };
+  in
+  {
+    devShells =
+      nixpkgs.lib.trivial.pipe ./. [
+        (dir: builtins.readDir dir)
+        (pairs: nixpkgs.lib.attrsets.filterAttrs (key: value: value == "directory") pairs)
+        (dirs: builtins.mapAttrs (shell: _: import (./. + "/${shell}") attrs) dirs)
+      ];
+  }
 )).devShells;
