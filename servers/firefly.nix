efa965c6cbd837189afa278294c72efe7772f3d7
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2022-10-14 06:15:08 -0300

    try to run firefly with localhost db

--- servers/firefly.nix
+++ servers/firefly.nix
@@ -6,10 +6,11 @@
     volumes = [
       "firefly_iii_upload:/var/www/html/storage/upload"
     ];
+    extraOptions = ["--network=host"];
     environment = {
       # < /dev/urandom tr -dc '[a-zA-Z][:digit:]' | head -c32
       "APP_KEY" = "opfUryV5FBAD7]yT[7py73IsqmU3M[lN";
-      "DB_HOST" = "10.88.0.1";
+      "DB_HOST" = "localhost";
       "DB_PORT" = "5432";
       "DB_CONNECTION" = "pgsql";
       "DB_DATABASE" = "firefly";
