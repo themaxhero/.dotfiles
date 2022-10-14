4450ec427efbc5b5840acbaf973267bde4bb2255
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2022-10-14 03:36:10 -0300

    add volumes and firefly port

--- servers/firefly.nix
+++ servers/firefly.nix
@@ -2,6 +2,10 @@
 {
   virtualisation.oci-containers.containers.firefly = {
     image = "fireflyiii/core:latest";
+    ports = ["6000:8080"];
+    volumes = [
+      "firefly_iii_upload:/var/www/html/storage/upload"
+    ];
     environment = {
       # < /dev/urandom tr -dc '[a-zA-Z][:digit:]' | head -c32
       "APP_KEY" = "opfUryV5FBAD7]yT[7py73IsqmU3M[lN";
