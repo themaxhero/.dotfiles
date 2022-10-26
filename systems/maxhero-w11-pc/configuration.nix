fc098b26b439717751e56ce048c747849294ada9
Marcelo A. de L. Santos <contact@maxhero.dev>
2022-10-26 11:21:28 -0300

    add maxhero-w11-pc

---
+++ systems/maxhero-w11-pc/configuration.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, lib, home-manager, nix-gaming, ... }:
+{
+  networking = {
+    hostId = "7ee3a466";
+    hostName = "maxhero-w11-pc";
+  };
+}
---
+++ systems/maxhero-w11-pc/default.nix
@@ -0,0 +1,22 @@
+{ nixpkgs, home-manager, nix-doom-emacs, ... }@attrs:
+nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  specialArgs = attrs;
+  modules = [
+    ../../modules/common
+    ../../modules/development
+    ../../modules/networking
+    ../../modules/sound
+    ../../modules/wireguard-client.nix
+    ./configuration.nix
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+    ({
+        home-manager.useGlobalPkgs = true;
+        home-manager.users.maxhero = nixpkgs.lib.mkMerge [
+        nix-doom-emacs.hmModule
+        (import ../../home/maxhero { seat = true; })
+        ];
+    })
+  ];
+}
