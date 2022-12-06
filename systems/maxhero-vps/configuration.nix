a54db9e2545fffc4a6a4d3160f87f0729ff08719
Marcelo A. de L. Santos <contact@maxhero.dev>
2022-12-06 16:34:00 -0300

    update vps home-manager

--- systems/maxhero-vps/configuration.nix
+++ systems/maxhero-vps/configuration.nix
@@ -60,5 +60,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
-  home-manager.users.maxhero.home.stateVersion = "21.11";
+  #home-manager.users.maxhero.home.stateVersion = "21.11";
 }
