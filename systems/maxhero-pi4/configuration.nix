cd1a55a9d200a3e79283dabfd6d8bff91fd51a58
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2023-04-06 12:46:48 -0300

    add samba server

--- systems/maxhero-pi4/configuration.nix
+++ systems/maxhero-pi4/configuration.nix
@@ -60,5 +60,42 @@
   };
   nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
   nix.registry.nixpkgs.flake = nixpkgs;
+  services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+  networking.firewall.allowedTCPPorts = [
+    5357 # wsdd
+  ];
+  networking.firewall.allowedUDPPorts = [
+    3702 # wsdd
+  ];
+  services.samba.openFirewall = true;
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user
+      #use sendfile = yes
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.15. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      public = {
+        path = "/mnt/sda1";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
+    };
+  };
   system.stateVersion = "23.05";
 }
