2b02940342b46a45917284a7b5c47a220918957b
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2023-04-18 10:19:45 -0300

    start cd/dvd installation to maxhero-workstation

--- systems/maxhero-workstation/default.nix
+++ systems/maxhero-workstation/default.nix
@@ -1,17 +1,19 @@
-{ nixpkgs, nix-doom-emacs, home-manager, ... }@attrs:
+{ self, nixpkgs, nix-doom-emacs, home-manager, ... }@attrs:
 nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   specialArgs = attrs;
   modules = [
-    ../../modules/common
-    ../../modules/development
-    ../../modules/graphical-interface
-    ../../modules/gaming
-    ../../modules/networking
-    ../../modules/sound
-    ../../modules/vfio
-    ../../modules/wireguard-client.nix
-    ../../modules/bare-metal
+    (nixpkgs + /nixos/modules/installer/cd-dvd/installation-cd-minimal.nix)
+    (nixpkgs + /nixos/modules/installer/cd-dvd/channel.nix)
+    (self + /modules/common)
+    (self + /modules/development)
+    (self + /modules/graphical-interface)
+    (self + /modules/gaming)
+    (self + /modules/networking)
+    (self + /modules/sound)
+    (self + /modules/vfio)
+    (self + /modules/wireguard-client.nix)
+    (self + /modules/bare-metal)
     ./configuration.nix
     ./hardware-configuration.nix
     home-manager.nixosModules.home-manager
@@ -19,7 +21,7 @@ nixpkgs.lib.nixosSystem {
       home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
-        users.maxhero = ../../home/maxhero;
+        users.maxhero = self + /home/maxhero;
         extraSpecialArgs = attrs // {
           inherit nix-doom-emacs;
           nixosConfig = config;
