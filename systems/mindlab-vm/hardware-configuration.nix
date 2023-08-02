9fa5d2d129fa7c56d584905fbfe844ffd19ab889
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2023-08-02 00:46:09 -0300

    fix kernel vm

--- systems/mindlab-vm/hardware-configuration.nix
+++ systems/mindlab-vm/hardware-configuration.nix
@@ -6,12 +6,7 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
   swapDevices = [ ];
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with “networking. interfaces.<interface>.useDHCP .
   networking.useDHCP = lib.mkDefault true;
-  # networking. interfaces.enp0s3.useDHCP = lib.mkDefault true;
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-Linux";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   virtualisation.virtualbox.guest.enable = true;
 }
