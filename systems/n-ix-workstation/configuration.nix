2a94b0509a59212d6fb144e8b90adbc65c96ed47
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2023-08-03 16:16:04 -0300

    add n-ix-workstation

---
+++ systems/n-ix-workstation/configuration.nix
@@ -0,0 +1,52 @@
+{ self, pkgs, ... }:
+{
+  networking = {
+    hostId = "cc1f83c4";
+    hostName = "n-ix-workstation";
+  };
+  services.xserver.serverFlagsSection = ''
+    Option "BlankTime" "0"
+    Option "StandbyTime" "0"
+    Option "SuspendTime" "0"
+    Option "OffTime" "0"
+  '';
+  development = {
+    enable = true;
+    languages = [
+     "dotnet"
+     "crystal"
+     "f#"
+     "ocaml"
+     # "elm"
+     "elixir"
+     "web"
+     "zig"
+     "node"
+     "ruby"
+     "scala"
+     "haskell"
+     "clojure"
+     "rust"
+     "android"
+     "aws"
+     "clasp"
+     "oracle-cloud"
+     "devops"
+     "kubernetes"
+    ];
+  };
+  services.xserver.windowManager.i3.extraSessionCommands = ''
+  fcitx5 &
+  '';
+  graphical-interface.enable = true;
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/858f7daa-36ea-4d5c-9bce-01d85b77053f";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/6784-CFF4";
+    fsType = "vfat";
+  };
+  system.stateVersion = "22.11";
+}
---
+++ systems/n-ix-workstation/default.nix
@@ -0,0 +1,31 @@
+{ self, nixpkgs, nix-doom-emacs, home-manager, nixos-hardware, ... }@attrs:
+nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  specialArgs = attrs;
+  modules = [
+    # TODO: Add Media Generation later
+    #(nixpkgs + /nixos/modules/installer/cd-dvd/installation-cd-minimal.nix)
+    #(nixpkgs + /nixos/modules/installer/cd-dvd/channel.nix)
+    nixos-hardware.nixosModules.lenovo-thinkpad-e14-amd
+    (self + /modules/common)
+    (self + /modules/development)
+    (self + /modules/graphical-interface)
+    (self + /modules/networking)
+    (self + /modules/sound)
+    (self + /modules/bare-metal)
+    ./configuration.nix
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+    ({ config, ... }: {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.maxhero = self + /home/maxhero;
+        extraSpecialArgs = attrs // {
+          inherit nix-doom-emacs;
+          nixosConfig = config;
+        };
+      };
+    })
+  ];
+}
