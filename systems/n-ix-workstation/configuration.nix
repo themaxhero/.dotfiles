d820f74fea1afe2ffcc9ff79a52e1c028f88c52c
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2023-08-04 01:43:40 -0300

    update n-ix-workstation config

--- systems/n-ix-workstation/configuration.nix
+++ systems/n-ix-workstation/configuration.nix
@@ -38,14 +38,28 @@
   services.xserver.windowManager.i3.extraSessionCommands = ''
   fcitx5 &
   '';
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = {
+    crypted = {
+      device = "/dev/disk/by-uuid/240f6901-3677-4024-8905-e13eceb642ca";
+      preLVM = true;
+    };
+  };
+
   graphical-interface.enable = true;
+
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/858f7daa-36ea-4d5c-9bce-01d85b77053f";
+    device = "/dev/disk/by-uuid/d9e63ed5-d194-43e2-8c82-4d521356086f";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/6784-CFF4";
+    device = "/dev/disk/by-uuid/245E-CF52";
     fsType = "vfat";
   };
   system.stateVersion = "22.11";
