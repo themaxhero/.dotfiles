636f11e2cfed18b24266232bcd95055557ac4712
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2023-09-14 04:53:06 -0300

    transmit containers over wg0

--- servers/minecraft-server-1.nix
+++ servers/minecraft-server-1.nix
@@ -3,7 +3,7 @@
   networking.nat = {
     enable = true;
     internalInterfaces = ["ve-+"];
-    externalInterfaces = ["enp0s3", "wg0"];
+    externalInterfaces = ["enp0s3" "wg0"];
     # Lazy IPv6 connectivity for the container
     enableIPv6 = true;
   };
