7e5244f18051a2f4b7aba47d0ab85297fbea3b61
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2023-12-16 07:39:05 -0300

    add lsp-zero

--- home/maxhero/development/nvim/plugin/treesitter.lua
+++ home/maxhero/development/nvim/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "help", "javascript", "typescript", "elixir", "erlang", "c", "rust", "lua"},
+  -- ensure_installed = { "help", "javascript", "typescript", "elixir", "erlang", "c", "rust", "lua"},
   sync_install = false,
   auto_install = false,
   highlight = {
