93a7295f195cfd88705c2958ab2c58abce8f0a12
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2024-03-03 20:22:31 -0300

    improve mkHome

--- home/maxhero/graphical-interface/i3/default.nix
+++ home/maxhero/graphical-interface/i3/default.nix
@@ -4,37 +4,35 @@ let
   spawnables = import (self + /home/maxhero/graphical-interface/spawnables) attrs;
 in
 {
-  config = lib.mkIf specialArgs.nixosConfig.graphical-interface.enable {
-    xsession.windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-      config = {
-        terminal = spawnables.xorg.terminal;
-        modifier = i3SwayCommon.modifier;
-        assigns = { };
-        bars = [ ];
-        colors = { };
-        focus.followMouse = true;
-        fonts = {
-          names = [ "scientifica" ];
-          size = 8.0;
-        };
-        keybindings = (i3SwayCommon.i3AndSwayKeybindings "xorg");
-        floating.criteria = i3SwayCommon.floatingCriteria;
-        window = i3SwayCommon.window;
-        modes = lib.mkOptionDefault {
-          "command_mode" = i3SwayCommon.commandMode;
-        };
-        # Could be the same as sway if I find programs that behave the same between Xorg and Wayland
-        startup = [
-          { command = "--no-startup-id ${spawnables.xorg.notification-daemon}"; }
-          { command = "--no-startup-id ${spawnables.xorg.bar}"; always = true; }
-          { command = "--no-startup-id ${spawnables.xorg.ime}"; }
-          { command = "--no-startup-id ${spawnables.xorg.wallpaper}"; }
-          { command = "--no-startup-id ${spawnables.xorg.network-applet}"; }
-          #{ command = "--no-startup-id ${clipman}"; }
-        ];
+  xsession.windowManager.i3 = {
+    enable = true;
+    package = pkgs.i3-gaps;
+    config = {
+      terminal = spawnables.xorg.terminal;
+      modifier = i3SwayCommon.modifier;
+      assigns = { };
+      bars = [ ];
+      colors = { };
+      focus.followMouse = true;
+      fonts = {
+        names = [ "scientifica" ];
+        size = 8.0;
       };
+      keybindings = (i3SwayCommon.i3AndSwayKeybindings "xorg");
+      floating.criteria = i3SwayCommon.floatingCriteria;
+      window = i3SwayCommon.window;
+      modes = lib.mkOptionDefault {
+        "command_mode" = i3SwayCommon.commandMode;
+      };
+      # Could be the same as sway if I find programs that behave the same between Xorg and Wayland
+      startup = [
+        { command = "--no-startup-id ${spawnables.xorg.notification-daemon}"; }
+        { command = "--no-startup-id ${spawnables.xorg.bar}"; always = true; }
+        { command = "--no-startup-id ${spawnables.xorg.ime}"; }
+        { command = "--no-startup-id ${spawnables.xorg.wallpaper}"; }
+        { command = "--no-startup-id ${spawnables.xorg.network-applet}"; }
+        #{ command = "--no-startup-id ${clipman}"; }
+      ];
     };
   };
 }
