042c664ed9af969452b8caadd17ddc2a0048ce7a
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2024-03-24 17:21:23 -0300

    center window name in bar

--- home/maxhero/graphical-interface/eww/modules/window_name.lua
+++ home/maxhero/graphical-interface/eww/modules/window_name.lua
@@ -10,8 +10,12 @@ function print_window_name()
   handle = io.popen(subscribe_to_window)
   local output = handle:read('*a')
   local event = json.decode(output)
+  local window = {
+    ["name"] = event["container"]["name"],
+    ["display"] = event["container"]["output"]
+  }
   if event then
-    print(event["container"]["name"])
+    print(json.encode(window))
   end
 end
 
